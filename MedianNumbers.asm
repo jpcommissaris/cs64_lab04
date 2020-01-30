88293119d307ced6738abaf210d86718f7344052
Julian Commissaris <jpcommissaris@gmail.com>
2020-01-29 23:30:20 -0800

    first 2

--- MedianNumbers.asm
+++ MedianNumbers.asm
@@ -1,23 +1,83 @@
 # MedianNumbers.asm program
 # CS 64, Z.Matni, zmatni@ucsb.edu
-#
+# Julian Commissaris & Rick Han
 
 .data
 
-	# TODO: Complete these declarations / initializations
+	str1: .asciiz "Enter the next number:\n"
+	str2: .asciiz "Median: "
 
-	.asciiz "Enter the next number:\n"
-
-	.asciiz "Median: "
-
-
-#Text Area (i.e. instructions)
 .text
 
 main:
+	# print "enter a number" 
+	li $v0, 4
+	la $a0, str1
+	syscall
 
-	# TODO: Write your code here
+	# get int
+	li $v0, 5
+	syscall
+	move $t0, $v0
+
+	# print "enter a number" 
+	li $v0, 4
+	la $a0, str1
+	syscall
+
+	# get int
+	li $v0, 5
+	syscall
+	move $t1, $v0
+
+	# print "enter a number" 
+	li $v0, 4
+	la $a0, str1
+	syscall
+
+	# get int
+	li $v0, 5
+	syscall
+	move $t2, $v0
+
+	#check 0 > 1
+	bgt $t0, $t1, bigger
+	# if 1 > 0 
+	bgt $t0, $t2, return0
+	bgt $t1, $t2, return2
+	j return1
+
+
+# if 0 > 1
+bigger:
+	bgt $t1, $t2, return1
+	bgt $t0, $t2, return2
+	j return0  
+
+return0:
+	move $t4, $t0
+	j exit 
+return1:
+	move $t4, $t1
+	j exit 
+return2:
+	move $t4, $t2
+	j exit 
+
 
 exit:
-	# TODO: Write code to properly exit a SPIM simulation
+
+	# print "median" 
+	li $v0, 4
+	la $a0, str2
+	syscall
+
+	# print ans
+	li $v0, 1
+	move $a0, $t4
+	syscall
+
+	# Exit
+	li $v0, 10
+	syscall
 
--- factorial.asm
+++ factorial.asm
@@ -11,15 +11,74 @@
 #   }
 #   cout << "Factorial of " << x << " is:\n" << fn << "\n";
 #
+
+
 .data
 
-	# TODO: Write your initializations here
+	str1: .asciiz "Enter a number:\n"
+	str2: .asciiz "Factorial of "
+	str3: .asciiz " is:\n"
+	str4: .asciiz " \n"
 
-#Text Area (i.e. instructions)
 .text
-main:
 
-	# TODO: Write your code here
+main:
+	# print "enter a number" 
+	li $v0, 4
+	la $a0, str1
+	syscall
+
+	# get int
+	li $v0, 5
+	syscall
+	move $t0, $v0  #t0 is the number to factorial
+	move $t5, $v0
+	move $t6, $v0
+
+#check if zero
+	beq $zero, $t0, set1
+
+
+loop:
+
+	sgt $t1, $t0, 2
+	beq $t1, $zero, exit
+	add $t0, $t0, -1
+
+	mult $t5, $t0 
+	mflo $t5 
+
+	j loop
+
+set1:
+	li $t5, 1
 
 exit:
-	# TODO: Write code to properly exit a SPIM simulation
+
+	#print strs
+	li $v0, 4
+	la $a0, str2
+	syscall
+
+	#print strs
+	li $v0, 1
+	move $a0, $t6
+	syscall
+
+	li $v0, 4
+	la $a0, str3
+	syscall
+
+
+	# print ans
+	li $v0, 1
+	move $a0, $t5
+	syscall
+	li $v0, 4
+	la $a0, str4
+	syscall
+
+	# Exit
+	li $v0, 10
+	syscall
+
